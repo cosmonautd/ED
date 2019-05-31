b78c79ecd21ae362410e55240c3de6603ecada46
David Borges <david.borges@protonmail.com>
2019-05-31 07:06:53 -0300

    Remoção de parâmetros

--- 3. Algoritmos Genéticos/genetic.py
+++ 3. Algoritmos Genéticos/genetic.py
@@ -1,15 +1,14 @@
 import tsp as model
 
-def base_algorithm(pop_size, elite_size, max_generations, 
-                        crossover_rate=0.9, mutation_rate=0.01):
+def base_algorithm(pop_size, elite_size, max_generations):
     population = model.init(pop_size)
     yield 0, population, model.fit(population)
     for g in range(max_generations):
         fitness = model.fit(population)
         elite = model.elitism(population, fitness, elite_size)
         parents = model.selection(population, fitness, pop_size - elite_size)
-        children = model.crossover(parents, crossover_rate)
-        children = model.mutation(children, mutation_rate)
+        children = model.crossover(parents)
+        children = model.mutation(children)
         population = elite + children
         yield g+1, population, model.fit(population)
         if model.stop(): break
